9082c7101889720711bc4b6e9c0fc9042af5d29e
lameeus <laurens.meeus@ugent.be>
2018-01-29 16:05:29 +0100

    * added lr decay * adjusted for training-, testing-, -validation date

--- f2018_01/unet_main.py
+++ f2018_01/unet_main.py
@@ -11,12 +11,14 @@ def main():
     parser = argparse.ArgumentParser(description="* Network on *")
     parser.add_argument('--dataset', default='art',
                         help="The dataset")
-    parser.add_argument('--epochs', default=500, type=int)
+    parser.add_argument('--epochs', default=50000, type=int)
     parser.add_argument('--lr', default=1e-3, type=float,
                         help="Initial learning rate")
+    parser.add_argument('--decay', default=1e-6, type=float,
+                        help="learning rate decay over EACH UPDATE.")
     parser.add_argument('--model', default='complex_unet',
                         help="The model type")
-    parser.add_argument('--save_dir', default='/home/lameeus/data/general/weights/simple2')     # where to save
+    parser.add_argument('--save_dir', default='/home/lameeus/data/general/weights/unet_complex_testing')     # where to save
     parser.add_argument('-t', '--testing', action='store_true',
                         help="Test the trained model on testing dataset")
     parser.add_argument('-w', '--weights', default=None,
@@ -27,7 +29,7 @@ def main():
     # load data
     if args.dataset == 'art':
         ext_tot = 26     # 6 for non-shift
-        (x_train, y_train), (x_test, y_test) = data_sets.load_art(ext_tot=ext_tot)
+        (x_train, y_train), (x_test, y_test), (x_val, y_val)= data_sets.load_art(ext_tot=ext_tot)
     elif args.dataset == 'mnist':
         (x_train, y_train), (x_test, y_test) = data_sets.load_mnist()
     else:
@@ -62,7 +64,7 @@ def main():
     else:  # as long as weights are given, will run testing
         if args.weights is None:
             print('No weights are provided. Will test using random initialized weights.')
-        train_net.test(model=model, data=(x_test, y_test), args=args)
+        train_net.test(model=model, data=(x_val, y_val), args=args)
 
 
 if __name__ == "__main__":
